02c24eb322ead67132632d79306c907ecf7b6d2a
Espresso38 <naj.tomalak@gmail.com>
2024-08-30 12:07:01 +0200

    Creating API request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css'
+
+fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London?key=CDXBQ75CSMH2BRWZQM7WC9RFF', {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        const description = response.description;
+        const conditions = response.currentConditions.conditions;
+        const temp = response.currentConditions.temp;
+        const feelslike = response.currentConditions.feelslike;
+        console.log(description, conditions, temp, feelslike);
+    });
