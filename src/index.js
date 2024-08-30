205f4dc128f3a8bb6338c363eddb4b6458608c79
Espresso38 <naj.tomalak@gmail.com>
2024-08-30 13:23:20 +0200

    Get weather conditions based on user input

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import './styles.css'
+import './styles.css';
 
-fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London?key=CDXBQ75CSMH2BRWZQM7WC9RFF', {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        const description = response.description;
-        const conditions = response.currentConditions.conditions;
-        const temp = response.currentConditions.temp;
-        const feelslike = response.currentConditions.feelslike;
-        console.log(description, conditions, temp, feelslike);
-    });
+const weatherForm = document.getElementById('weather_form');
+const cityInput = document.getElementById('city_name');
+const img = document.getElementById('img_condition');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const cityName = cityInput.value.trim().toLowerCase();
+
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?key=CDXBQ75CSMH2BRWZQM7WC9RFF`, { mode: 'cors' })
+        .then(response => response.json())
+        .then(data => {
+            let address = data.address;
+            address = address.charAt(0).toUpperCase() + address.slice(1);
+            const description = data.description;
+            const conditions = data.currentConditions.conditions;
+            const temp = data.currentConditions.temp;
+            const tempCelcius = Math.round((temp - 32)/(1.8));
+            const feelslike = data.currentConditions.feelslike;
+            const feelslikeCelcius = Math.round((feelslike - 32)/(1.8));
+            
+            console.log('Address:', address);
+            console.log('Description:', description);
+            console.log('Conditions:', conditions);
+            console.log('Temperature:', tempCelcius);
+            console.log('Feels like:', feelslikeCelcius);
+            
+        })
+
+});
